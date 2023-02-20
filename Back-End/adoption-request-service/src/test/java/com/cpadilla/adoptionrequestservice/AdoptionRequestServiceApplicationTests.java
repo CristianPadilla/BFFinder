854d550827884760333a9747414f4dd24e822401
Cristian Padilla <lozadacristian21@gmail.com>
2023-02-19 22:19:47 -0500

    creando servicios
    se crean los servicios por ahora necesarios con sus respectivas dependencias

---
+++ Back-End/adoption-request-service/src/test/java/com/cpadilla/adoptionrequestservice/AdoptionRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpadilla.adoptionrequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
